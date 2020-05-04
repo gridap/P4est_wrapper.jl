2815898c97c17b3983e9e11e89ac708ccbd2e813
victorsndvg <victorsv@gmail.com>
2020-05-04 13:57:07 +0200

    change error message. Remove travis documentation stage

--- src/p4est_wrapper.jl
+++ src/p4est_wrapper.jl
@@ -6,12 +6,12 @@ module p4est_wrapper
 # Load in `deps.jl`, complaining if it does not exist
 const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
 if !isfile(depsjl_path)
-    error("p4est.jl was not build properly. Please run Pkg.build(\"p4est.jl\").")
+    error("p4est_wrapper was not build properly. Please run Pkg.build(\"p4est.jl\").")
 end
 include(depsjl_path)
 # Check if p4est library was found
 if(!P4EST_FOUND)
-    error("p4est.jl was not build properly. Please run Pkg.build(\"p4est.jl\").")
+    error("p4est_wrapper was not build properly. Please run Pkg.build(\"p4est.jl\").")
 end
 
 ################################################################
