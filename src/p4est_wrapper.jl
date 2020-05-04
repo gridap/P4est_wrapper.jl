76cbffcfc42d94ec65bf497c1e14ddf163d35c91
victorsndvg <victorsv@gmail.com>
2020-05-04 12:10:08 +0200

    Add content to main project file and ctypes.jl file

--- src/p4est_wrapper.jl
+++ src/p4est_wrapper.jl
@@ -1,5 +1,47 @@
 module p4est_wrapper
 
-greet() = print("Hello World!")
+################################################################
+# Check correct installation
+################################################################
+# Load in `deps.jl`, complaining if it does not exist
+const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if !isfile(depsjl_path)
+    error("p4est.jl was not build properly. Please run Pkg.build(\"p4est.jl\").")
+end
+include(depsjl_path)
+# Check if p4est library was found
+if(!P4EST_FOUND)
+    error("p4est.jl was not build properly. Please run Pkg.build(\"p4est.jl\").")
+end
+
+################################################################
+# Use dependencies 
+################################################################
+using Libdl
+using MPI
+using CEnum
+
+################################################################
+# Include p4est wrappers
+################################################################
+include("ctypes.jl")
+export Ctm, Ctime_t, Cclock_t
+
+# Use library path from build process
+const p4est_lib = P4EST_LIB
+
+# Include wrappers from 
+include(joinpath(@__DIR__, "..", "gen", "p4est_fixes.jl"))
+include(joinpath(@__DIR__, "..", "gen", "p4est_common.jl"))
+include(joinpath(@__DIR__, "..", "gen", "p4est_api.jl"))
+
+################################################################
+# Export everything
+################################################################
+foreach(names(@__MODULE__, all=true)) do s
+    if startswith(string(s), "sc") || startswith(string(s), "p4est") || startswith(string(s), "p6est") || startswith(string(s), "p8est") 
+        @eval export $s
+    end
+end
 
 end # module
