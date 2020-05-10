1b31b31fd4c747ca624f6017a632964afff24d8a
victorsndvg <victorsv@gmail.com>
2020-05-10 12:58:37 +0200

    Implement sc_array_index function in fixes.jl file

--- gen/fixes.jl
+++ gen/fixes.jl
@@ -20,9 +20,10 @@ struct sc_array
     elem_size::ssize_t
     elem_count::ssize_t
     byte_alloc::ssize_t
-    array::Cstring
+    array::Ptr{Cchar}
 end
 
+const sc_array_t = sc_array
 ################################################################
 # Non supported data/struct/unions
 ################################################################
@@ -43,4 +44,14 @@ const p8est_lnodes_t = ud
 const p8est_quadrant_data = ud
 const p8est_iter_edge_side_data = ud
 const p8est_iter_face_side_data = ud
+
+################################################################
+# Array index functions
+################################################################
+
+function sc_array_index(array::Ptr{sc_array_t}, iz::ssize_t)
+    sc_array_object = unsafe_load(array)
+    @assert iz < sc_array_object.elem_count
+    return sc_array_object.array + sc_array_object.elem_size * iz
+end 
 
