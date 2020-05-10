3d0b56a5017dd521771f4eb801a2ae58892cd59e
victorsndvg <victorsv@gmail.com>
2020-05-10 12:59:14 +0200

    Improve MPI initialization management from tests. Test sc_array_index function. Improve p4est_example.jl test comments. Test is self-contained.

--- test/p4est_example.jl
+++ test/p4est_example.jl
@@ -1,7 +1,20 @@
+using MPI
 using p4est_wrapper
+using Test
 
+# Initialize MPI if not initialized yet
+if !MPI.Initialized()
+    MPI.Init()
+end
+
+#############################################################################
+# Definition of data structures and function callbacks
+#############################################################################
+
+# Point struct
 struct point_t x::Cdouble; y::Cdouble; end
 
+# Refine callback
 function my_refine( ::Ptr{p4est_t}, which_tree::p4est_topidx_t, quadrant::Ptr{p4est_quadrant_t})
     @assert which_tree == 0
     q = unsafe_load(quadrant)
@@ -9,30 +22,62 @@ function my_refine( ::Ptr{p4est_t}, which_tree::p4est_topidx_t, quadrant::Ptr{p4
     return Cint(0)
 end
 
+# C-callable refine callback
 const my_refine_c = @cfunction(my_refine, Cint, (Ptr{p4est_t}, p4est_topidx_t, Ptr{p4est_quadrant_t}))
 
+# Search callback function
 function search_point_fn(::Ptr{p4est_t}, ::p4est_topidx_t, ::Ptr{p4est_quadrant_t}, local_num::p4est_locidx_t, point::Ptr{Cvoid})
     @info local_num, unsafe_load(convert(Ptr{point_t}, point))
     return Cint(1)
 end
 
+# C-callable search callback function
 const search_point_fn_c = @cfunction(search_point_fn, Cint, (Ptr{p4est_t}, p4est_topidx_t, Ptr{p4est_quadrant_t}, p4est_locidx_t, Ptr{Cvoid}))
 
-p4est_conn = p4est_connectivity_new_unitsquare() 
-@test p4est_conn != C_NULL
-p4est_ = p4est_new(MPI.COMM_WORLD, p4est_conn, 0, C_NULL, C_NULL) 
-@test p4est_ != C_NULL
-p4est_refine(p4est_, 1, my_refine_c, C_NULL)
-ptr_to_points_array = sc_array_new_count(sizeof(point_t), 2)
+#############################################################################
+# Main program
+#############################################################################
+
+# Create a connectivity structure for the unit square.
+unitsquare_connectivity = p4est_connectivity_new_unitsquare() 
+@test unitsquare_connectivity != C_NULL
+
+# Create a new forest
+unitsquare_forest = p4est_new(MPI.COMM_WORLD, unitsquare_connectivity, 0, C_NULL, C_NULL) 
+@test unitsquare_forest != C_NULL
+
+# Register callback function to decide for refinement. 
+p4est_refine(unitsquare_forest, 1, my_refine_c, C_NULL)
+
+# Create a new sc_array
+ptr_to_points_sc_array = sc_array_new_count(sizeof(point_t), 2)
+@test ptr_to_points_sc_array != C_NULL
+
+# Check correct number of elements and its size in sc_array
+points_sc_array = unsafe_load(ptr_to_points_sc_array)
+@test points_sc_array.elem_count == 2 && points_sc_array.elem_size == sizeof(point_t)
+
+# Obtain reference to first point_t 
+ptr_to_points_array = convert(Ptr{point_t}, sc_array_index(ptr_to_points_sc_array,0))
 @test ptr_to_points_array != C_NULL
-points_array = unsafe_load(ptr_to_points_array)
-@test points_array.elem_count == 2 && points_array.elem_size == sizeof(point_t)
-ptr_to_point = convert(Ptr{point_t},pointer(points_array.array))
-@test ptr_to_point != C_NULL
-unsafe_store!(ptr_to_point, point_t(0.24, 0.24), 1)
-unsafe_store!(ptr_to_point, point_t(0.51, 0.256), 2)
-p4est_search(p4est_, C_NULL, search_point_fn_c, ptr_to_points_array)  
-p4est_vtk_write_file(p4est_, C_NULL, "my_step")
-sc_array_destroy(ptr_to_points_array)
-p4est_destroy(p4est_)
-p4est_connectivity_destroy(p4est_conn)
+
+# Overwrite point_t values in sc_array
+unsafe_store!(ptr_to_points_array, point_t(0.24, 0.24), 1)
+unsafe_store!(ptr_to_points_array, point_t(0.51, 0.256), 2)
+
+# Search through quadrants, the local part of a forest, or the partition.
+p4est_search(unitsquare_forest, C_NULL, search_point_fn_c, ptr_to_points_sc_array)
+# Write forest to VTK file
+p4est_vtk_write_file(unitsquare_forest, C_NULL, "my_step")
+
+# Destroy sc_array structure
+sc_array_destroy(ptr_to_points_sc_array)
+# Destroy the forest
+p4est_destroy(unitsquare_forest)
+# Destroy the connectivity
+p4est_connectivity_destroy(unitsquare_connectivity)
+
+# Finalize MPI if initialized and session is not interactive
+if (MPI.Initialized() && !isinteractive())
+    MPI.Finalize()
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,5 @@ using Test
 
 # Skip tests if library was not properly loaded
 if p4est_wrapper.P4EST_FOUND
-    using MPI
-    if !MPI.Initialized()
-        MPI.Init()
-    end
-
     @testset "p4est example" begin include("p4est_example.jl") end
-
-    if (MPI.Initialized() && !isinteractive())
-        MPI.Finalize()
-    end
 end
