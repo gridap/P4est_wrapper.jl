cbd21fe3233a44e4d3da477563c809c5d3d3a91f
Alberto F. Martin <alberto.martin@monash.edu>
2024-03-27 21:23:44 +1100

    Renaming. Adding lnodes tests to runtests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,4 +45,15 @@ if P4est_wrapper.P4EST_FOUND
         @show cmd
         run(cmd)
     end
+    @testset "Example lnodes 2D" begin
+        cmd = `$mpiexec -n $nprocs $(extra_args) $(julia) --project=$repodir $(joinpath(dir, "example_lnodes.jl"))`
+        @show cmd
+        run(cmd)
+    end
+    @testset "Example lnodes 3D" begin
+        cmd = `$mpiexec -n $nprocs $(extra_args) $(julia) --project=$repodir $(joinpath(dir, "example_lnodes_3d.jl"))`
+        @show cmd
+        run(cmd)
+    end
+
 end
